ea2fdd63dfbaa6f6c8f9a048a47dde51319157c2
aidgamesgg <ya.ivanov1302@yandex.ru>
2022-09-30 17:47:13 +0700

    first

---
+++ yoptaplus.hpp
@@ -0,0 +1,24 @@
+#ifndef YOPTAPLUS__
+#define YOPTAPLUS__
+
+#define гы int
+#define моча {
+#define гавно }
+#define залупа (
+#define пизда )
+#define спиздить using
+#define залупу namespace
+#define семки std
+#define выебатся cout
+#define кончить endl
+#define вмазать return
+#define нихуя false
+#define дохуя true
+#define семечник main
+#define воткнуть <<
+#define проебать for
+#define уебан <
+#define похуист >
+#define впиздеть =
+
+#endif
